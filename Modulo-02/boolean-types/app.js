20be4039400ad8a391de52eef1f22636bd86d201
Gabriel Carneiro <gabriel29music@gmail.com>
2021-12-12 19:33:03 -0300

    boolean types em TS

---
+++ Modulo-02/boolean-types/app.js
@@ -0,0 +1,13 @@
+"use strict";
+//exemplo 01
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log(tarefaConcluida, tarefaPendente);
+//exemplo 02
+let concluido = false;
+if (concluido) {
+    console.log("Tarefa concluída com sucesso!");
+}
+else {
+    console.log("Tarefa pendente");
+}
---
+++ Modulo-02/boolean-types/app.ts
@@ -0,0 +1,14 @@
+//exemplo 01
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida, tarefaPendente);
+
+//exemplo 02
+let concluido: boolean = false;
+
+if(concluido) {
+    console.log("Tarefa concluída com sucesso!")
+}else{
+    console.log("Tarefa pendente")
+}
