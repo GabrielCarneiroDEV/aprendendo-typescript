578f4ab8d29fad7c7573256739db0298d9a539e6
Gabriel Carneiro <gabriel29music@gmail.com>
2021-12-12 15:31:31 -0300

    Primeiro commit - hello world

---
+++ Modulo-01/hello-world/app.js
@@ -0,0 +1,13 @@
+"use strict";
+const mensagem = "Hello world!";
+console.log(mensagem);
+/*ao ser compilado é gerado um arquivo chamado "app.js" este arquivo então pode ser executado normalmente como um arquvio javascript.
+Pra isso foi utilizado o comando 'tsc ./app.ts'.
+
+
+Para que não seja necessário repetir o mesmo processo várias vezes para todos os arquvios .ts podemos configurar o arquvio tsconfig.json. Da segunte forma:
+
+No terminal na raiz do projeto:
+tsc --init
+
+*/ 
---
+++ Modulo-01/hello-world/app.ts
@@ -0,0 +1,17 @@
+const mensagem: string =  "Hello world!";
+
+console.log(mensagem)
+
+/*ao ser compilado é gerado um arquivo chamado "app.js" este arquivo então pode ser executado normalmente como um arquvio javascript.  
+Pra isso foi utilizado o comando 'tsc ./app.ts'.
+
+
+Para que não seja necessário repetir o mesmo processo várias vezes para todos os arquvios .ts podemos configurar o arquvio tsconfig.json. Da segunte forma:
+
+No terminal na raiz do projeto:
+tsc --init
+
+-- o arquivo tsconfig.json será criado.
+
+Agora utilizamos ctrl+shift+b e clickamos em compilar tsconfig.json
+*/
