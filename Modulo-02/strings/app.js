209a725332cd2498235f817b6562ad03be7367cc
Gabriel Carneiro <gabriel29music@gmail.com>
2021-12-12 20:06:48 -0300

    tipo string

---
+++ Modulo-02/strings/app.js
@@ -0,0 +1,6 @@
+"use strict";
+//exemplo 01 - aspas simples (single quotes)
+let nomeCompleto = 'Gabriel Carneiro';
+console.log(nomeCompleto);
+//exemplo 02 - aspas duplas (double quotes)
+//exmplo 03 - crase (Back ticks)
---
+++ Modulo-02/strings/app.ts
@@ -0,0 +1,6 @@
+//exemplo 01 - aspas simples (single quotes)
+let nomeCompleto: string = 'Gabriel Carneiro';
+console.log(nomeCompleto)
+//exemplo 02 - aspas duplas (double quotes)
+//exmplo 03 - crase (Back ticks)
+
