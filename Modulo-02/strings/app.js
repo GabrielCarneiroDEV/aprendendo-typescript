1751e54b79fd4169535b9a8f1d21899005f59e4f
Gabriel Carneiro <gabriel29music@gmail.com>
2021-12-13 18:31:19 -0300

    estrura do código refatorada

--- Modulo-02/strings/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//exemplo 01 - aspas simples (single quotes)
-let nomeCompleto = 'Gabriel Carneiro';
-console.log(nomeCompleto);
-//exemplo 02 - aspas duplas (double quotes)
-let funcao = "Estudante de análise e desenvolvimento de sistemas";
-console.log(funcao);
-//exmplo 03 - crase (Back ticks)
-let mensagemAoUsuario = `Seja bem ${nomeCompleto} você é ${funcao}.`;
