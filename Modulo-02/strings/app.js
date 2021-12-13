fcb027cbaf8364be892694648e4d36ee04595c77
Gabriel Carneiro <gabriel29music@gmail.com>
2021-12-12 21:18:17 -0300

    strings

--- Modulo-02/strings/app.js
+++ Modulo-02/strings/app.js
@@ -3,4 +3,7 @@
 let nomeCompleto = 'Gabriel Carneiro';
 console.log(nomeCompleto);
 //exemplo 02 - aspas duplas (double quotes)
+let funcao = "Estudante de análise e desenvolvimento de sistemas";
+console.log(funcao);
 //exmplo 03 - crase (Back ticks)
+let mensagemAoUsuario = `Seja bem ${nomeCompleto} você é ${funcao}.`;
--- Modulo-02/strings/app.ts
+++ Modulo-02/strings/app.ts
@@ -1,6 +1,11 @@
 //exemplo 01 - aspas simples (single quotes)
 let nomeCompleto: string = 'Gabriel Carneiro';
-console.log(nomeCompleto)
+console.log(nomeCompleto);
+
 //exemplo 02 - aspas duplas (double quotes)
+let funcao:string = "Estudante de análise e desenvolvimento de sistemas";
+console.log(funcao);
+
 //exmplo 03 - crase (Back ticks)
+let mensagemAoUsuario:string = `Seja bem ${nomeCompleto} você é ${funcao}.`
 
